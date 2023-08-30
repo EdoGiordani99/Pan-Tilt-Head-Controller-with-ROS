633e91158fdba6cde917ceeb10cbcd69d78bf6e2
Edoardo Giordani <giordaniedoardo99@gmail.com>
2023-08-31 00:25:37 +0200

    Added Final Scripts

---
+++ src/servocontrol/scripts/example/subscriber.cpp
@@ -0,0 +1,55 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include "servocontrol/state.h"
+#include <thread>
+#include <cstdlib>
+
+using namespace std;
+
+
+ros::Publisher state_pub;
+ros::Subscriber cmd_sub;
+
+void cmdCallback(const servocontrol::state &state) {
+    cout << "Received servo command:  " << state.last_cmd << endl;    
+    state_pub.publish(state);
+
+    ros::spinOnce();
+}
+
+void activateConnection()
+{
+
+    while(ros::ok() && cmd_sub.getNumPublishers() == 0)
+    {
+    servocontrol::state state;
+
+    state.last_cmd = "[Controller]: Activating Connection";
+    state.step = 0;
+    state.speed = 0.0;
+    state.x = 0;
+    state.y = 0;
+    state_pub.publish(state);
+
+    ros::spinOnce();
+    ros::Duration(0.5).sleep();
+    cout << "Waiting for connection with COMMANDER" << endl;
+    }
+}
+
+
+int main(int argc, char** argv) {
+
+    ros::init(argc, argv, "Controller");
+    ros::NodeHandle nh;
+
+    state_pub = nh.advertise<servocontrol::state>("servo_state", 1000);
+    cmd_sub = nh.subscribe("servo_cmd", 1000, cmdCallback);
+
+    //activateConnection();
+
+    ros::spin();
+
+    return 0;
+}
